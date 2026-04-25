1748050a227e2aa0244e05812d5db29d713252b4
Devraj070 <debarajmalik05@gmail.com>
2026-04-25 16:44:10 +0530

    err solved for next.config

--- next.config.mjs
+++ next.config.mjs
@@ -8,21 +8,36 @@
 
 
 
-import withPWA from "next-pwa";
+import withPWAInit from "next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactCompiler: true,
-};
 
-export default withPWA({
-  ...nextConfig,
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
+  allowedDevOrigins: ["192.168.1.13"],
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+    ],
   },
 
-  // 👇 IMPORTANT for Next 16 + Turbopack compatibility
+  // 🔥 CRITICAL FIX FOR NEXT 16 + TURBOPACK
   turbopack: {},
-});
+
+  // optional safety
+  experimental: {},
+};
+
+export default withPWA(nextConfig);
